40af85a2bca4c694f07cb800715774a2c65ec9d4
Renan1404 <82480715+Renan1404@users.noreply.github.com>
2023-04-04 20:21:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const Dúvida = document.getElementById("Dúvida");
-const Conclusão = document.getElementById("Conclusão");
+const duvida = document.getElementById("duvida");
+const conclusao = document.getElementById("conclusao");
 
-Conclusão.addEventListener("keypress", (e) => {
-  if (Dúvida.value && e.key === "Enter") SendQuestion();
+duvida.addEventListener("keypress", (e) => {
+  if (duvida.value && e.key === "Enter") {
+    SendQuestion();
+  }
 });
 
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "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";
 
 function SendQuestion() {
-  var sQuestion = Dúvida.value;
+  var sQuestion = duvida.value;
 
   fetch("https://api.openai.com/v1/completions", {
     method: "POST",
@@ -26,32 +28,36 @@ function SendQuestion() {
   })
     .then((response) => response.json())
     .then((json) => {
-      if (Conclusão.value) Conclusão.value += "\n";
+      if (conclusao.value){
+        conclusao.value += "\n";
+      } 
 
       if (json.error?.message) {
-        Conclusão.value += `Error: ${json.error.message}`;
+        conclusao.value += `Error: ${json.error.message}`;
       } else if (json.choices?.[0].text) {
         var text = json.choices[0].text || "Sem resposta";
+       conclusao.value += "Mestre: " + text;
 
-        Conclusão.value += "Mestre: " + text;
       }
 
-      Conclusão.scrollTop = Conclusão.scrollHeight;
+      conclusao.scrollTop = conclusao.scrollHeight;
     })
     .catch((error) => console.error("Error:", error))
     .finally(() => {
-      Conclusão.value = "";
-      Conclusão.disabled = false;
-      Conclusão.focus();
+      conclusao.value = "";
+      conclusao.disabled = false;
+      conclusao.focus();
     });
 
-  if (Conclusão.value) Conclusão.value += "\n\n\n";
 
-  Dúvida.value += `Eu: ${sQuestion}`;
-  Conclusão.value = "Esperando resposta...";
-  Conclusão.disabled = true;
+    conclusao.value += `Eu: ${sQuestion} \n`;
+    conclusao.value += "Esperando resposta... \n";
+    conclusao.value += `${text}`;
 
-  Conclusão.scrollTop = Conclusão.scrollHeight;
+    conclusao.scrollTop = conclusao.scrollHeight;
+
+
+  
 }
 
 let slideIndex = 1;
