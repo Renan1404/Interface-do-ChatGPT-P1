7a05b822ba9b36692e699cf2f01d6e3aaf06bc29
Renan1404 <82480715+Renan1404@users.noreply.github.com>
2023-04-04 21:32:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const duvida = document.getElementById("duvida");
 const conclusao = document.getElementById("conclusao");
-
 duvida.addEventListener("keypress", (e) => {
   if (duvida.value && e.key === "Enter") {
    SendQuestion();
   }
 });
-
-const OPENAI_API_KEY = "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";
-
+const OPENAI_API_KEY = "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";
 const SendQuestion = async() => {
+  conclusao.value = '';
   var sQuestion = duvida.value;
-
   await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
@@ -22,58 +19,30 @@ const SendQuestion = async() => {
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
       messages:[{"role": "user", "content": sQuestion}],
-      max_tokens: 2048, // Tamanho
-      temperature: 1, // Criatividade 
+      max_tokens: 2048,
+      temperature: 1, 
     }),
-  })
-    .then((response) => response.json())
+  }).then((response) => response.json())
     .then((json) => {
-      //console.log(json);
-      if (conclusao.value){
-        conclusao.value += "\n";
-      } 
-
-      /*if (json.error?.message) {
-        conclusao.value += `Error: ${json.error.message}`;
-      } else if (json.choices?.[0].text) {
-*/
       var text = json.choices[0].message.content ;
-      console.log(text);
+
       conclusao.value += "Mestre: " + text;
-
-    //}
-
+      document.getElementById('duvida').value = '';
       conclusao.scrollTop = conclusao.scrollHeight;
     })
     .catch((error) => console.error("Error:", error))
     .finally(() => {
-      conclusao.value = "";
-      conclusao.disabled = false;
-      conclusao.focus();
     });
-
-
-    /*conclusao.value += `Eu: ${sQuestion} \n`;
-    conclusao.value += "Esperando resposta... \n";
-    conclusao.value += text;
-*/
     conclusao.scrollTop = conclusao.scrollHeight;
-
-
-  
 }
-
 let slideIndex = 1;
 showSlides(slideIndex);
-
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
